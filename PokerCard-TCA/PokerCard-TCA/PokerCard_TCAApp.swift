df568e2bd5f9b742a1153d374f1ea7533bdd918a
Paulo Roberto <paulora999@gmail.com>
2023-02-15 13:40:43 -0300

    First Commit Start Project

---
+++ PokerCard-TCA/PokerCard-TCA/PokerCard_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokerCard_TCAApp.swift
+//  PokerCard-TCA
+//
+//  Created by Paulo Roberto on 15/02/23.
+//
+
+import SwiftUI
+
+@main
+struct PokerCard_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
